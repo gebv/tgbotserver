57f0259dd90d4e9be9a98068053b57a3db36ce9d
G B <egor.workroom@gmail.com>
2016-08-16 19:11:42 +0600

    added configuranor

---
+++ apps/appconfig/src/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Sirupsen/logrus"
+)
+
+var NAME = "appconfig"
+var VERSION = "0.0.1"
+var APIEntrypoint = "https://api.telegram.org/bot%s/%s"
+
+func main() {
+	webhook := os.Getenv("TGWEBHOOK")
+	token := os.Getenv("TGTOKEN")
+	hostname := os.Getenv("HOSTNAME")
+	logLevelStr := os.Getenv("LOGLEVEL")
+
+	logLevel, err := logrus.ParseLevel(logLevelStr)
+	if err != nil {
+		logLevel = logrus.ErrorLevel
+	}
+	logrus.SetLevel(logLevel)
+
+	logrus.WithFields(logrus.Fields{
+		"_ref":  NAME,
+		"_host": hostname,
+
+		"version":  VERSION,
+		"logLevel": logLevelStr,
+		"webhook":  webhook,
+		"tgtoken":  len(token),
+	}).Infoln("httplistener init")
+
+	if err := setWebhook(token, webhook); err != nil {
+		logrus.WithFields(logrus.Fields{
+			"_ref": NAME,
+			"err":  err,
+		}).Fatal("set webhook")
+	}
+
+	signalChan := make(chan os.Signal, 2)
+	done := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-signalChan
+		logrus.WithFields(logrus.Fields{
+			"_ref":  NAME,
+			"_host": hostname,
+		}).Info("signal completion of the process")
+
+		if err := setWebhook(token, ""); err != nil {
+			logrus.WithFields(logrus.Fields{
+				"_ref": NAME,
+				"err":  err,
+			}).Fatal("clear webhook")
+		}
+		done <- true
+	}()
+	<-done
+
+	os.Exit(0)
+
+}
+
+func setWebhook(token, webhook string) error {
+	apiurl := fmt.Sprintf(APIEntrypoint, token, "setWebhook")
+
+	api, err := url.Parse(apiurl)
+	query := api.Query()
+	query.Add("url", webhook)
+	api.RawQuery = query.Encode()
+
+	req, err := http.NewRequest("POST", api.String(), nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+
+		return err
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"_ref": NAME,
+		"err":  err,
+	}).Info("successfully updated webhook")
+
+	return nil
+}
